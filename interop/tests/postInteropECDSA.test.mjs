faa36bfff28eabd1cf5a72fdf124c6bbaae027af
Curtis Deptuck <curtdept@me.com>
2022-10-18 18:32:57 -0600

    split tests into common parts

--- interop/tests/postInteropECDSA.test.mjs
+++ interop/tests/postInteropECDSA.test.mjs
@@ -35,30 +35,6 @@ describe("Check interop ECDSA routes", () => {
       body: fs.readFileSync("./tests/cert/test-public.key"),
     });
   });
-  describe("POST /interop basic tests", () => {
-    test(`should return ${httpStatus.BAD_REQUEST} without a body`, async () => {
-      const token = await genToken();
-      const res = await request(app)
-        .post("/interop")
-        .set("Authorization", token)
-        .send();
-
-      expect(res.statusCode).toEqual(httpStatus.BAD_REQUEST);
-    });
-
-    test(`should return ${httpStatus.BAD_REQUEST} with an invalid action`, async () => {
-      const token = await genToken();
-      const body = {
-        action: "testme",
-      };
-
-      await request(app)
-        .post("/interop")
-        .set("Authorization", token)
-        .send(body)
-        .expect(httpStatus.BAD_REQUEST);
-    });
-  });
   describe("POST /interop provision tests", () => {
     let thingName;
 
@@ -66,26 +42,6 @@ describe("Check interop ECDSA routes", () => {
       let cert = await getCertificate(Buffer.from(testCert, "base64"));
       let testSerialNum = await getSerialNumber(cert);
       thingName = `${thingNamePrefix}${testSerialNum}`;
-    });
-
-    test(`should return ${httpStatus.BAD_REQUEST} with provision action with invalid (non-base64) cert`, async () => {
-      const token = await genToken();
-      const body = {
-        action: "provision",
-        certs: [
-          {
-            ref: "a",
-            cert: "acbdefghijklmnopqrstuvwxyz1234",
-          },
-        ],
-      };
-
-      const res = await request(app)
-        .post("/interop")
-        .set("Authorization", token)
-        .send(body);
-
-      expect(res.statusCode).toEqual(httpStatus.BAD_REQUEST);
     });
 
     test(`should return ${httpStatus.BAD_REQUEST} for input without reference`, async () => {
@@ -440,42 +396,6 @@ describe("Check interop ECDSA routes", () => {
           message: "Cannot decode certitifcate",
         },
       ]);
-    });
-
-    test(`should return ${httpStatus.OK} with status action`, async () => {
-      const token = await genToken();
-      const body = {
-        action: "status",
-      };
-
-      const res = await request(app)
-        .post("/interop")
-        .set("Authorization", token)
-        .send(body);
-
-      expect(res.statusCode).toEqual(httpStatus.OK);
-      expect(res.body).toEqual({
-        message: "Not yet implemented.",
-        action: "status",
-      });
-    });
-
-    test(`should return ${httpStatus.OK} with message action`, async () => {
-      const token = await genToken();
-      const body = {
-        action: "message",
-      };
-
-      const res = await request(app)
-        .post("/interop")
-        .set("Authorization", token)
-        .send(body);
-
-      expect(res.statusCode).toEqual(httpStatus.OK);
-      expect(res.body).toEqual({
-        message: "Not yet implemented.",
-        action: "message",
-      });
     });
   });
 });
